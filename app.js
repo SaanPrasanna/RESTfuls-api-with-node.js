afcc295f3a157b87190b2b2d07020b0bc898f94d
SaanPrasanna <30783645+SaanPrasanna@users.noreply.github.com>
2021-02-26 19:52:20 +0530

    First Commit

--- app.js
+++ app.js
@@ -6,6 +6,4 @@ const userRouter = require("./api/users/user.router");
 app.use(express.json());
 
 app.use("/api/users",userRouter);
-app.listen(process.env.APP_PORT,()=>{
-    console.log("Server up and running on port: ", process.env.APP_PORT);
-})
+app.listen(process.env.PORT)
